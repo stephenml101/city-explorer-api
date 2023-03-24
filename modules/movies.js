906d778813508f134d86f6b5b1a262de956fece0
Stephen <stephenlevesque2@gmail.com>
2023-03-24 14:10:08 -0700

    lab 10

--- modules/movies.js
+++ modules/movies.js
@@ -1,30 +1,56 @@
 'use strict'
 
-const axios =require('axios');
+const axios = require('axios');
 
-async function getMovies(request,response, next) {
+let cache = {};
+
+// TODO: create a key for the data I am going to store
+// TODO: if the thing exists and within a valid timeframe, send that data from cache
+// TODO: if the thing does not exist - call my API and cache that return from the
+
+async function getMovies(request, response, next) {
   try {
     //TODO: ACCEPT MY QUERIES
     let cityFromFrontEnd = request.query.city_name;
-    // TODO: BUILD MY URL FOR AXIOS
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${cityFromFrontEnd}`;
-    let movieResults = await axios.get(url);
 
-    // TODO: GROOM THAT DATA TO SEND TO FRONTEND
-    let moviesToSend = movieResults.data.results.map(movie => {
-      return new Movies(movie);
-    });
+    let key = `${cityFromFrontEnd}-Movie`; // key = Seattle-Movie
 
-    response.status(200).send(moviesToSend);
+    if (cache[key] && (Date.now() - cache[key].timestamp) < 10000) {
+      console.log('Movies cache was hit!')
+
+      response.status(200).send(cache[key].data)
+    } else {
+
+      console.log('No items in cache');
+
+      let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${cityFromFrontEnd}`;
+      // TODO: BUILD MY URL FOR AXIOS
+      let movieResults = await axios.get(url);
+
+      // TODO: GROOM THAT DATA TO SEND TO FRONTEND
+      let moviesToSend = movieResults.data.results.map(movie => {
+        return new Movies(movie);
+      });
+
+      // BUILD INTO CACHE
+      cache[key] = {
+        data: moviesToSend,
+        timestamp: Date.now()
+      };
+
+      response.status(200).send(moviesToSend);
+    }
+
+
   } catch (error) {
     next(error);
   }
 }
 
 class Movies {
-  constructor(movieObj){
-    this.title=movieObj.original_title;
-    this.overview=movieObj.overview;
+  constructor(movieObj) {
+    this.title = movieObj.original_title;
+    this.overview = movieObj.overview;
     this.image = `https://image.tmdb.org/t/p/w500${movieObj.poster_path}`;
   }
 }
